b938961460aaf21aa17d14aaa4a0196521388369
KyryloCA <107942772+KyryloCA@users.noreply.github.com>
2023-01-28 15:41:41 +0000

    Deploying to gh-pages from @ KyryloCA/goit-js-hw-08@d0e3d4c61df17f8c95f22fbdcc295502b7ac9b04 🚀

--- 02-video.a3da9737.js
+++
@@ -1,2 +0,0 @@
-console.log("привет"),console.log("hello");
-//# sourceMappingURL=02-video.a3da9737.js.map
--- 02-video.a6fe9466.js
+++
@@ -1,2 +0,0 @@
-console.log("привет"),console.log("hello");
-//# sourceMappingURL=02-video.a6fe9466.js.map
